f62ad926bdc458c2f195ee01f63d7302a8b7aaed
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-06-28 12:12:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var input = document.getElementById("myFile");
+var output = document.getElementById("output");
+
+
+input.addEventListener("change", function () {
+  if (this.files && this.files[0]) {
+    var myFile = this.files[0];
+    var reader = new FileReader();
+    
+    reader.addEventListener('load', function (e) {
+      output.textContent = e.target.result;
+    });
+    
+    reader.readAsBinaryString(myFile);
+  }   
+});
