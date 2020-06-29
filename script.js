fb55a9efd5d5ca84a4af6018c5143e5b33b23048
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-06-29 15:12:35 -0300

    Changed loadFile into a function

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var input = document.getElementById("myFile");
-var output = document.getElementById("output");
-
-
-input.addEventListener("change", function () {
-  if (this.files && this.files[0]) {
-    var myFile = this.files[0];
-    var reader = new FileReader();
+function loadFile(){
+  var input = document.getElementById("myFile");
+  var output = document.getElementById("output");
     
-    reader.addEventListener('load', function (e) {
-      output.textContent = e.target.result;
-    });
-    
-    reader.readAsBinaryString(myFile);
-  }   
-});
+  input.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      var myFile = this.files[0];
+      var reader = new FileReader();
+      
+      reader.addEventListener('load', function (e) {
+        output.textContent = e.target.result;
+      });
+      
+      reader.readAsBinaryString(myFile);
+    }   
+  });
+}
+loadFile();
