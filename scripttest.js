976aba627b3d9312138294aec124d79862b7900b
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-06-29 18:00:06 -0300

    Testing some stuffs

--- scripttest.js
+++ scripttest.js
@@ -18,26 +18,36 @@ function loadFile(){
 loadFile();
 
 function tableCreate(){
-  var body = document.body,
+  var body = document.getElementById("myFile"),
   tbl  = document.createElement('table');
   tbl.style.width  = '100px';
   tbl.style.border = '1px solid black';
 
-  for(var i = 0; i < 3; i++){
-      var tr = tbl.insertRow();
-      for(var j = 0; j < 2; j++){
-          if(i == 2 && j == 1){
-              break;
-          } else {
-              var td = tr.insertCell();
-              td.appendChild(document.createTextNode('Cell'));
-              td.style.border = '1px solid black';
-              if(i == 1 && j == 1){
-                  td.setAttribute('rowSpan', '2');
-              }
-          }
-      }
-  }
-  body.appendChild(tbl);
+  input.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      var myFile = this.files[0];
+      var reader = new FileReader();
+      
+      for(var i = 0; i < 3; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j < 2; j++){
+            if(i == 2 && j == 1){
+                break;
+            } else {
+                var td = tr.insertCell();
+                td.appendChild(document.createTextNode('Cell'));
+                td.style.border = '1px solid black';
+                if(i == 1 && j == 1){
+                    td.setAttribute('rowSpan', '1');
+                }
+            }
+        }
+    }
+    body.appendChild(tbl);
+
+    }   
+  });
+
+  
 }
 tableCreate();
