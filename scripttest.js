f33ec4ae71b94acf17cb66762afa30ff46abfe7e
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-06-29 15:12:46 -0300

    Add a test script

---
+++ scripttest.js
@@ -0,0 +1,43 @@
+function loadFile(){
+  var input = document.getElementById("myFile");
+  var output = document.getElementById("output");
+    
+  input.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      var myFile = this.files[0];
+      var reader = new FileReader();
+      
+      reader.addEventListener('load', function (e) {
+        output.textContent = e.target.result;
+      });
+      
+      reader.readAsBinaryString(myFile);
+    }   
+  });
+}
+loadFile();
+
+function tableCreate(){
+  var body = document.body,
+  tbl  = document.createElement('table');
+  tbl.style.width  = '100px';
+  tbl.style.border = '1px solid black';
+
+  for(var i = 0; i < 3; i++){
+      var tr = tbl.insertRow();
+      for(var j = 0; j < 2; j++){
+          if(i == 2 && j == 1){
+              break;
+          } else {
+              var td = tr.insertCell();
+              td.appendChild(document.createTextNode('Cell'));
+              td.style.border = '1px solid black';
+              if(i == 1 && j == 1){
+                  td.setAttribute('rowSpan', '2');
+              }
+          }
+      }
+  }
+  body.appendChild(tbl);
+}
+tableCreate();
