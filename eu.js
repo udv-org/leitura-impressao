c8b66a11e0d098d71c4a61b1f8cd98994f8ec939
Chrystofer Jost da Rosa <chrysjost@gmail.com>
2020-07-01 22:57:03 -0300

    Arquivo de Teste

---
+++ eu.js
@@ -0,0 +1,18 @@
+function loadFile(){
+    var input = document.getElementById("myFile");
+    var output = document.getElementById("output");
+      
+    input.addEventListener("change", function () {
+      if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+        
+        reader.addEventListener('load', function (e) {
+          output.textContent = e.target.result;
+        });
+        
+        reader.readAsBinaryString(myFile);
+      }   
+    });
+  }
+  loadFile();
